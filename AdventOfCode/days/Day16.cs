fe5d4a913dd941b87033edade3b815932ce25d5e
Christian Syska <belua85@gmail.com>
2016-08-29 22:10:55 +0200

    Day 16 input and class

---
+++ AdventOfCode/days/Day16.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace AdventOfCode.Days
+{
+    public class Day16 : Day
+    {
+        public Day16() : base(16) { }
+                
+        public override string GetSolutionPart1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetSolutionPart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
