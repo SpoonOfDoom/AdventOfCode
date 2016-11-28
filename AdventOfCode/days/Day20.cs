90108ad4c5bf5c85ca95e5e3b1201bf79ecd6ec9
Christian Syska <syska.christian@gmail.com>
2016-11-28 10:45:55 +0100

    Day 20, some helper methods and naive approach

--- AdventOfCode/days/Day20.cs
+++ AdventOfCode/days/Day20.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using AdventOfCode.Extensions;
+using System.Threading.Tasks;
 
 namespace AdventOfCode.Days
 {
@@ -8,10 +10,82 @@ namespace AdventOfCode.Days
     {
         public Day20() : base(20) { }
 
+        private Dictionary<int,int> housePresents = new Dictionary<int, int>();
+        private int targetPresentCount = 36000000;
+
+        private static bool IsDivisible(int number, int divider)
+        {
+            return number%divider == 0;
+        }
+
+        private static IEnumerable<int> GetDivisors(int number)
+        {
+            var candidates = Enumerable.Range(1, number);
+            return candidates.Where(x => IsDivisible(number, x));
+        }
+
+        private int CalculateHousePresents(int houseNumber)
+        {
+            var divisors = GetDivisors(houseNumber);
+            int presents = divisors.Sum(divisor => divisor*10);
+            housePresents[houseNumber] = presents;
+
+            return presents;
+        }
+
+        private static int GetRangeSum(int number)
+        {
+            var numbers = Enumerable.Range(0, number);
+            return numbers.Sum(i => i*10);
+        }
+
+        private int GetFirstTargetHouse()
+        {
+            int currentStart = 1;
+            const int batchSize = 1000;
+            bool found = false;
+
+            for (int i = 0; i < targetPresentCount; i++)
+            {
+                int rangeSum = GetRangeSum(i);
+                if (rangeSum >= targetPresentCount)
+                {
+                    currentStart = i;
+                    break;
+                }
+            }
+            currentStart = targetPresentCount/100;
+            int maxPresentsFound = 0;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            while (!found)
+            {
+                Console.Clear();
+                Console.WriteLine("Current start: {0}", currentStart);
+                var candidates = Enumerable.Range(currentStart, batchSize);
+                Console.WriteLine("Max presents found: {0}", maxPresentsFound);
+                Console.WriteLine("Difference to target: {0}", targetPresentCount - maxPresentsFound);
+                Console.WriteLine("Time: {0}", sw.Elapsed);
+                Parallel.ForEach(candidates, house =>
+                {
+                    int presents = CalculateHousePresents(house);
+                    maxPresentsFound = Math.Max(presents, maxPresentsFound);
+
+                    if (presents >= targetPresentCount)
+                    {
+                        found = true;
+                    }
+                });
+
+                currentStart += batchSize;
+            }
+            return -1;
+        }
         
         public override string GetSolutionPart1()
         {
-            return base.GetSolutionPart1();
+            var result = GetFirstTargetHouse();
+            return result.ToString();
         }
 
         public override string GetSolutionPart2()
