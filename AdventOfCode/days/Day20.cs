e2256785da7b6d5cda109629f6229bfa9cbaec04
Christian Syska <syska.christian@gmail.com>
2016-11-30 13:55:55 +0100

    naive recursive approach - stack overflow

--- AdventOfCode/days/Day20.cs
+++ AdventOfCode/days/Day20.cs
@@ -11,6 +11,7 @@ namespace AdventOfCode.Days
         public Day20() : base(20) { }
 
         private Dictionary<int,int> housePresents = new Dictionary<int, int>();
+		private HashSet<string> startedSarches = new HashSet<string>();
         private int targetPresentCount = 36000000;
         private int lowestPossible;
 
@@ -42,25 +43,38 @@ namespace AdventOfCode.Days
 
         private bool SearchHouse(int low, int high, ref int lowestFound, int level = 0)
         {
+	        string searchString = $"{low}-{high}";
+	        if (startedSarches.Contains(searchString))
+	        {
+		        return false;
+	        }
+	        else
+	        {
+		        startedSarches.Add(searchString);
+	        }
+			Console.Clear();
+	        Console.WriteLine("Level: {0}", level);
+			Console.WriteLine("Lowest found: {0}", lowestFound);
+	        Console.WriteLine("Numbers checked: {0}", housePresents.Count);
+	        Console.WriteLine("Searches started: {0}", startedSarches.Count);
             if (lowestFound < low)
             {
                 return false;
             }
-            if (lowestFound < high)
-            {
-                high = lowestFound;
-            }
-            int mid = (low + high)/2;
+			if (lowestFound < high)
+			{
+				high = lowestFound;
+			}
+			int mid = (low + high)/2;
             if (lowestFound < mid)
             {
-                return SearchHouse(low, lowestFound, ref lowestFound, level + 1);
+				return SearchHouse(low, lowestFound, ref lowestFound, level + 1);
             }
             if (housePresents.ContainsKey(mid))
             {
                  //dirty, dirty, temporary hack
                 if (high == low+1)
                 {
-                    
                     mid = mid + 1;
                     if (housePresents.ContainsKey(mid))
                     {
@@ -75,23 +89,36 @@ namespace AdventOfCode.Days
             }
             
             int presentCount = CalculateHousePresents(mid);
-            housePresents[mid] = presentCount;
             if (presentCount >= targetPresentCount)
             {
-                lowestFound = Math.Min(lowestFound, mid);
-                SearchHouse(1, mid, ref lowestFound, level + 1);
-                
+	            lowestFound = Math.Min(lowestFound, mid);
+
+				return SearchHouse(lowestPossible, Math.Min(mid, lowestFound), ref lowestFound, level + 1);
             }
             else
             {
                 if (lowestFound < int.MaxValue)
                 {
-                    SearchHouse(low, mid, ref lowestFound, level + 1);
+	                if (mid < lowestFound)
+	                {
+						SearchHouse(low, mid, ref lowestFound, level + 1);
+		                if (mid < lowestFound)
+		                {
+							return SearchHouse(mid, lowestFound, ref lowestFound, level + 1);
+						}
+					}
+	                else
+	                {
+						return SearchHouse(low, mid, ref lowestFound, level + 1);
+					}
                 }
                 else
                 {
-                    SearchHouse(mid, high, ref lowestFound, level + 1);
-                    SearchHouse(low, mid, ref lowestFound, level + 1);
+	                if (mid < lowestFound)
+	                {
+						SearchHouse(mid, Math.Min(high, lowestFound), ref lowestFound, level + 1);
+					}
+	                return SearchHouse(low, mid, ref lowestFound, level + 1);
                 }
             }
             return true;
@@ -118,8 +145,8 @@ namespace AdventOfCode.Days
             Stopwatch sw = new Stopwatch();
             sw.Start();
             int lowestFound = int.MaxValue;
-            found = SearchHouse(currentStart, targetPresentCount/10, ref lowestFound);
-
+            found = SearchHouse(currentStart, targetPresentCount, ref lowestFound);
+	        found = SearchHouse(currentStart, lowestFound, ref lowestFound);
             if (found)
             {
                 return housePresents.Where(x => x.Value >= targetPresentCount).Select(x => x.Key).Min();
