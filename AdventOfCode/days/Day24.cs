e43975a4769c98f5686040a55b7dead661879b05
Christian Syska <syska.christian@gmail.com>
2017-01-20 15:18:18 +0100

    new approach wip

--- AdventOfCode/days/Day24.cs
+++ AdventOfCode/days/Day24.cs
@@ -78,6 +78,7 @@ namespace AdventOfCode.Days
             public string VerboseInfo => $"Remaining presents: {remainingPresents.Count}    \n";
 
             public Queue<int> remainingPresents = new Queue<int>();
+            public static int targetWeight;
 
             public PresentState()
             {
@@ -107,9 +108,7 @@ namespace AdventOfCode.Days
 
             private bool WeightCheck()
             {
-                return PresentGroups[0].Weight == PresentGroups[1].Weight
-                       && PresentGroups[0].Weight == PresentGroups[2].Weight
-                       && PresentGroups[1].Weight == PresentGroups[2].Weight;
+                return PresentGroups.All(group => group.Weight == targetWeight);
             }
 
             private bool LegRoomCheck()
@@ -130,6 +129,10 @@ namespace AdventOfCode.Days
                     }
                     PresentState newState = this.Clone();
                     newState.PresentGroups[i].AddPresent(newState.remainingPresents.Dequeue());
+                    if (newState.PresentGroups[i].Weight > targetWeight)
+                    {
+                        continue;
+                    }
 
                     actions.Add(new ExpandAction
                                 {
@@ -156,7 +159,7 @@ namespace AdventOfCode.Days
 
             public bool IsGoalState(ISearchNode goalState = null)
             {
-                return WeightCheck() && LegRoomCheck();
+                return remainingPresents.Count == 0 && WeightCheck() && LegRoomCheck();
             }
 
             public float GetHeuristic(ISearchNode goalState = null)
@@ -174,11 +177,125 @@ namespace AdventOfCode.Days
                 return heuristic + difference + (PresentGroups[0].QuantumEntanglement*1000);
             }
         }
-        
+
+        class IntListEqualityComparer : IEqualityComparer<List<int>>
+        {
+            public bool Equals(List<int> x, List<int> y)
+            {
+                if (x.Count != y.Count)
+                {
+                    return false;
+                }
+                for (int i = 0; i < x.Count; i++)
+                {
+                    if (x[i] != y[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            public int GetHashCode(List<int> obj)
+            {
+                int hash = 421337;
+                foreach (int i in obj)
+                {
+                    hash = hash * i.GetHashCode();
+                }
+                return hash;
+            }
+        }
+
+        private HashSet<List<int>> MakeTriplets(List<int> presents)
+        {
+            HashSet<List<int>> combinations = new HashSet<List<int>>(new IntListEqualityComparer());
+            Moep(combinations, new List<int>(), presents, 1);
+            //int targetWeight = totalWeight / 3;
+            
+            return combinations;
+        }
+
+
+        private void Moep(HashSet<List<int>> combinations,  List<int> combo, List<int> presents,  int level)
+        {
+            if (presents.Count == 0)
+            {
+                return;
+            }
+            if (combo.Sum() > totalWeight/3)
+            {
+                return;
+            }
+            else if (combo.Sum() == totalWeight / 3)
+            {
+                combo.Sort();
+                combinations.Add(combo);
+                return;
+            }
+            else
+            {
+                if ((totalWeight/3) - combo.Sum() > presents.Sum())
+                {
+                    //not enough presents left to reach target weight
+                    return;
+                }
+                foreach (int present in presents)
+                {
+                    List<int> newCombo = new List<int>(combo);
+                    newCombo.Add(present);
+                    int index = presents.IndexOf(present);
+                    Moep(combinations, newCombo, presents.GetRange(index+1, presents.Count -(index+1)), level + 1);
+                    
+                    //int missing = totalWeight / 3 - combo.Sum();
+                    //if (presents.Contains(missing))
+                    //{
+
+                    //}
+                }
+            }
+        }
+
+        private HashSet<Tuple<HashSet<int>, HashSet<int>, HashSet<int>>> GetCombinations(HashSet<List<int>> possiblePresentBags)
+        {
+            HashSet<Tuple<HashSet<int>, HashSet<int>, HashSet<int>>> ret = new HashSet<Tuple<HashSet<int>, HashSet<int>, HashSet<int>>>();
+            List<HashSet<int>> possibleBags = possiblePresentBags.OrderBy(p => p.Count).Select(p => new HashSet<int>(p)).ToList();
+            for (int i = 0; i < possibleBags.Count; i++)
+            {
+                HashSet<int> bag = possibleBags[i];
+                List<HashSet<int>> remainingList = possibleBags.GetRange(i + 1, possibleBags.Count - (i + 1))
+                    .Where(set => !set.Overlaps(bag)).ToList();
+
+                for (int j = 0; j < remainingList.Count; j++)
+                {
+                    HashSet<int> bag2 = remainingList[j];
+                    List<HashSet<int>> remainingList2 = remainingList.GetRange(j + 1, remainingList.Count - (j + 1))
+                        .Where(set => !set.Overlaps(bag2)).ToList();
+
+                    for (int k = 0; k < remainingList2.Count; k++)
+                    {
+                        HashSet<int> bag3 = remainingList2[k];
+
+                        Tuple<HashSet<int>, HashSet<int>, HashSet<int>> combo = Tuple.Create(bag, bag2, bag3);
+                        ret.Add(combo);
+                    }
+                }
+            }
+
+            return ret;
+        }
+
+
         public override string GetSolutionPart1()
         {
             allPresents.AddRange(inputLines.Select(line => line.ToInt()));
+            allPresents.Reverse();
             totalWeight = allPresents.Sum();
+            HashSet<List<int>> possiblePresentBags = MakeTriplets(allPresents);
+            HashSet<Tuple<HashSet<int>, HashSet<int>, HashSet<int>>> combinations = GetCombinations(possiblePresentBags);
+
+            
+            PresentState.targetWeight = totalWeight / 3;
             PresentState p = new PresentState {remainingPresents = new Queue<int>(allPresents)};
             int result = new AStar().GetMinimumCost(p, verbose: true);
 
